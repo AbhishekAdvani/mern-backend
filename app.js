ed2750c2a0ae61d8095efc9a9715eda0d9ca30d3
AbhishekAdvani <a9819191140@gmail.com>
2023-09-19 14:17:03 +0530

    changes user issue

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
-const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -19,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/places", placesRoutes); // => /api/places...
+app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
